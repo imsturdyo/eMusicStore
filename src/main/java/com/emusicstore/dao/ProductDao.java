c6827f7cfc057f36a7b66f99a988cedb125fd1bf
imsturdyo <imsturdyo@users.noreply.github.com>
2019-10-27 04:27:34 +0000

    configure database continue -add h2 dependency -add product implication

--- src/main/java/com/emusicstore/dao/ProductDao.java
+++ src/main/java/com/emusicstore/dao/ProductDao.java
@@ -2,64 +2,17 @@ package com.emusicstore.dao;
 
 import com.emusicstore.model.Product;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class ProductDao {
+public interface ProductDao {
 
-    private List<Product> productList;
+    void addProduct (Product product);
 
-    public List<Product> getProductList() {
-        Product product1 = new Product();
-        product1.setProductId("p123");
-        product1.setProductName("Guitar1");
-        product1.setProductCategory("Instrument");
-        product1.setProductDescription("This is a fender strat guitar!");
-        product1.setProductPrice(1200);
-        product1.setProductCondition("new");
-        product1.setProductStatus("Active");
-        product1.setUnitInStock(11);
-        product1.setProductManufacturer("Fender");
+    Product getProductById(String id);
 
-        Product product2 = new Product();
-        product2.setProductId("p124");
-        product2.setProductName("Record1");
-        product2.setProductCategory("Record");
-        product2.setProductDescription("Awesome mix!!");
-        product2.setProductPrice(25);
-        product2.setProductCondition("new");
-        product2.setProductStatus("Active");
-        product2.setUnitInStock(51);
-        product2.setProductManufacturer("EMI");
+    List<Product> getAllProducts();
 
-        Product product3 = new Product();
-        product3.setProductId("p125");
-        product3.setProductName("Speaker");
-        product3.setProductCategory("Accessories");
-        product3.setProductDescription("One of the best in the house");
-        product3.setProductPrice(55);
-        product3.setProductCondition("new");
-        product3.setProductStatus("Active");
-        product3.setUnitInStock(9);
-        product3.setProductManufacturer("Polk");
+    void deleteProduct(String id);
 
-        productList = new ArrayList<Product>();
-        productList.add(product1);
-        productList.add(product2);
-        productList.add(product3);
-
-        return productList;
-
-    }
-
-    public Product getProductById(String productId) throws IOException {
-        for (Product product : getProductList()) {
-            if (product.getProductId().equals(productId)) {
-                return product;
-            }
-        }
-
-        throw new IOException("No product found");
-    }
 }
+
