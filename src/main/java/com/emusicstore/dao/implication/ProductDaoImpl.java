d2bf1806f234db765890109f66bc386cea806623
imsturdyo <imsturdyo@users.noreply.github.com>
2019-11-09 21:07:22 +0000

    add edit product function

--- src/main/java/com/emusicstore/dao/implication/ProductDaoImpl.java
+++ src/main/java/com/emusicstore/dao/implication/ProductDaoImpl.java
@@ -24,6 +24,12 @@ public class ProductDaoImpl implements ProductDao {
         session.flush();
     }
 
+    public void editProduct(Product product) {
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(product);
+        session.flush();
+    }
+
     public Product getProductById(String id) {
         Session session = sessionFactory.getCurrentSession();
         Product product = (Product) session.get(Product.class, id);
@@ -47,5 +53,4 @@ public class ProductDaoImpl implements ProductDao {
         session.flush();
 
     }
-
 }
