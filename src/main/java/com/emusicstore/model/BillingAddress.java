11ddb10368128f5b5b44363212a657438a017e30
imsturdyo <imsturdyo@users.noreply.github.com>
2020-01-03 19:41:15 +0000

    add model classes

---
+++ src/main/java/com/emusicstore/model/BillingAddress.java
@@ -0,0 +1,92 @@
+package com.emusicstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+
+@Entity
+public class BillingAddress implements Serializable {
+
+    private static final long serialVersionUID = -6091579459463730482L;
+
+    @Id
+    @GeneratedValue
+    private int billingAddressId;
+    private String streetName;
+    private String doorNumber;
+    private String city;
+    private String county;
+    private String postcode;
+
+    @OneToOne
+    private Customer customer;
+
+    public int getBillingAddressId() {
+        return billingAddressId;
+    }
+
+    public void setBillingAddressId(int billingAddressId) {
+        this.billingAddressId = billingAddressId;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getDoorNumber() {
+        return doorNumber;
+    }
+
+    public void setDoorNumber(String doorNumber) {
+        this.doorNumber = doorNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "BillingAddress{" +
+                "streetName='" + streetName + '\'' +
+                ", doorNumber='" + doorNumber + '\'' +
+                ", city='" + city + '\'' +
+                ", county='" + county + '\'' +
+                ", postcode='" + postcode + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/emusicstore/model/ShippingAddress.java
@@ -0,0 +1,92 @@
+package com.emusicstore.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+
+@Entity
+public class ShippingAddress implements Serializable {
+
+    private static final long serialVersionUID = 989191150380037359L;
+
+    @Id
+    @GeneratedValue
+    private int shippingAddressId;
+    private String streetName;
+    private String doorNumber;
+    private String city;
+    private String county;
+    private String postcode;
+
+    @OneToOne
+    private Customer customer;
+
+    public int getShippingAddressId() {
+        return shippingAddressId;
+    }
+
+    public void setShippingAddressId(int shippingAddressId) {
+        this.shippingAddressId = shippingAddressId;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getDoorNumber() {
+        return doorNumber;
+    }
+
+    public void setDoorNumber(String doorNumber) {
+        this.doorNumber = doorNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "ShippingAddress{" +
+                "streetName='" + streetName + '\'' +
+                ", doorNumber='" + doorNumber + '\'' +
+                ", city='" + city + '\'' +
+                ", county='" + county + '\'' +
+                ", postcode='" + postcode + '\'' +
+                '}';
+    }
+}
