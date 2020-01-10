a3e6c56818917bfaf459cb5082946fae60c29f59
imsturdyo <imsturdyo@users.noreply.github.com>
2020-01-10 23:25:50 +0000

    add cartItem Service

---
+++ src/main/java/com/emusicstore/controller/CartResources.java
@@ -0,0 +1,66 @@
+package com.emusicstore.controller;
+
+import com.emusicstore.model.Cart;
+import com.emusicstore.model.CartItem;
+import com.emusicstore.model.Customer;
+import com.emusicstore.model.Product;
+import com.emusicstore.service.CartItemService;
+import com.emusicstore.service.CartService;
+import com.emusicstore.service.CustomerService;
+import com.emusicstore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/rest/cart")
+public class CartResources {
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @RequestMapping("/{cartId}")
+    public @ResponseBody Cart getCartById(@PathVariable (value = "cartId") int cartId) {
+        return cartService.getCartById(cartId);
+    }
+
+    @RequestMapping(value = "/add/{productId}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void addItem(@PathVariable(value = "productId") int productId, @AuthenticationPrincipal User activeUser) {
+
+        Customer customer = customerService.getCustomerByUsername(activeUser.getUsername());
+        Cart cart = customer.getCart();
+        Product product = productService.getProductById(productId);
+        List<CartItem> cartItems = cart.getCartItems();
+
+        for (int i = 0; i < cartItems.size(); i++) {
+            if (product.getProductId() == cartItems.get(i).getProduct().getProductId()) {
+                CartItem cartItem = cartItems.get(i);
+                cartItem.setQuantity(cartItem.getQuantity()+1);
+                cartItem.setTotalPrice(product.getProductPrice() * cartItem.getQuantity());
+                cartItemService.addCartItem(cartItem);
+            }
+        }
+
+        CartItem cartItem = new CartItem();
+        cartItem.setProduct(product);
+        cartItem.setQuantity(1);
+        cartItem.setTotalPrice(product.getProductPrice() * cartItem.getQuantity());
+        cartItem.setCart(cart);
+        cartItemService.addCartItem(cartItem);
+    }
+}
